6fa5376ae1d282dace62d8ec2af81c190b66c539
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-06-06 19:02:53 -0300

    agregue rutas a pantallas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import DetailPage from './pages/DetailPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/nubila" element={<LandingPage />} />
+        <Route path="/details/button1/" element={<DetailPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;

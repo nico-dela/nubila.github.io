656ef27d84de8ea1b6fd40405839aa8e24cf24f7
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-06-06 19:02:40 -0300

    agregue dos pantallas de ejemplo

---
+++ src/pages/DetailPage.js
@@ -0,0 +1,13 @@
+// DetailsPage.jsx
+import React from 'react';
+
+const DetailPage = () => {
+  return (
+    <div>
+      <h1>Detalles</h1>
+      {/* Agrega aquí el código JSX de tu página de detalles */}
+    </div>
+  );
+};
+
+export default DetailPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// HomePage.jsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Inicio</h1>
+      {/* Agrega aquí el código JSX de tu página de inicio */}
+    </div>
+  );
+};
+
+export default HomePage;

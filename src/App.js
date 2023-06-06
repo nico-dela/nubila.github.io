b2d452ec0acb527d77050dc3e0eabfd0928061f5
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-06-06 20:14:31 -0300

    agregue rutas a paginas principales

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import DetailPage from './pages/DetailPage';
+import MusicaPage from './pages/MusicaPage';
+import ProcesoPage from './pages/ProcesoPage';
+import CreditosPage from './pages/CreditosPage';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/nubila" element={<LandingPage />} />
-        <Route path="/details/button1/" element={<DetailPage />} />
+        <Route path="/nubila/home" element={<HomePage />} />
+        <Route path="/nubila/musica" element={<MusicaPage />} />
+        <Route path="/nubila/proceso" element={<ProcesoPage />} />
+        <Route path="/nubila/creditos" element={<CreditosPage />} />
       </Routes>
     </Router>
   );

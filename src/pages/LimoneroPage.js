a4716cea284ce7252cd19c27cb584b5819dea4df
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-07-11 22:11:31 -0300

    agregue nuevas paginas

---
+++ src/pages/LimoneroPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LimoneroPage = () => {
+  return (
+    <div>
+      <h1 style={{fontFamily: 'Bebas Neue, sans-serif'}}>Limonero</h1>
+      {/* Agrega aquí el código JSX de tu página de detalles */}
+      <Link to="/nubila" style={{ textDecoration: "none" }}>
+        <button
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#000",
+            transition: "transform 0.3s ease", // Transición suave
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+            fontFamily: "Courier New, monospace", // Especifica la fuente de texto aquí
+          }}
+          onMouseEnter={(e) => {
+            e.target.style.opacity = '0.5'; // Cambiar el color de fondo al hacer hover
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.opacity = '1'; // Restaurar el color de fondo al dejar de hacer hover
+          }}
+        >
+          Volver
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default LimoneroPage;

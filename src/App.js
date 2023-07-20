47db9e93914fa58302e458c5aef896214805131f
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-07-19 21:16:27 -0300

    agregue nuevas paginas y hoja de estilo

--- src/App.js
+++ src/App.js
@@ -7,19 +7,31 @@ import BoleritoPage from './pages/BoleritoPage';
 import MariposaPage from './pages/MariposaPage';
 import FrioPage from './pages/FrioPage';
 import LimoneroPage from './pages/LimoneroPage';
+import AcercaPage from './pages/AcercaPage';
+import CreditosPage from './pages/CreditosPage';
+import MusicPlayer from './components/MusicPlayer';
+import Menu from './components/Menu';
+import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/nubila" element={<LandingPage />} />
-        <Route path="/nubila/oceanica" element={<OceanicaPage />} />
-        <Route path="/nubila/girasoles" element={<GirasolesPage />} />
-        <Route path="/nubila/bolerito" element={<BoleritoPage />} />
-        <Route path="/nubila/mariposa" element={<MariposaPage />} />
-        <Route path="/nubila/frio" element={<FrioPage />} />
-        <Route path="/nubila/limonero" element={<LimoneroPage />} />
-      </Routes>
+      <div className="container">
+        <Menu />
+        <Routes>
+          <Route path="/nubila" element={<LandingPage />} />
+          <Route path="/nubila/oceanica" element={<OceanicaPage />} />
+          <Route path="/nubila/girasoles" element={<GirasolesPage />} />
+          <Route path="/nubila/bolerito" element={<BoleritoPage />} />
+          <Route path="/nubila/mariposa" element={<MariposaPage />} />
+          <Route path="/nubila/frio" element={<FrioPage />} />
+          <Route path="/nubila/limonero" element={<LimoneroPage />} />
+          <Route path="/nubila/creditos" element={<CreditosPage />} />
+          <Route path="/nubila/acerca" element={<AcercaPage />} />
+          <Route path="/nubila/colabora" element={<LimoneroPage />} />
+        </Routes>
+        <MusicPlayer />
+      </div>
     </Router>
   );
 };

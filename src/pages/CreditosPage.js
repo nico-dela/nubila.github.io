f7540f6f085c60ebe95ded954926958f4c950dfa
nico-dela <delacruzbordajn@gmail.com>
2023-10-07 22:35:01 -0300

    creacion y asociacion de hoja de estilo en creditos

--- src/pages/CreditosPage.js
+++ src/pages/CreditosPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../styles/CommonPage.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/CreditosPage.css";
 
 const CreditosPage = () => {
   return (

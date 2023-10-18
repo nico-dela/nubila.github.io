20a356bebc5a3afa4c55d37e827dd57a854fcbf6
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-10-17 23:57:59 -0300

    agregue fabicon para spotify

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,22 +1,37 @@
 import React from "react";
 import instagramIcon from "../assets/images/instagram.svg";
+import spotifyIcon from "../assets/images/spotify.svg";
 
 import "../styles/SocialMedia.css";
 
 const SocialMedia = () => {
   return (
-    <a
-      href="https://www.instagram.com/nubila___/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="instagram-link"
-    >
-      <img
-        src={instagramIcon}
-        alt="Instagram"
-        style={{ width: "40px", height: "40px" }} // Ajusta el tamaño según tus preferencias
-      />
-    </a>
+    <div id="main-container">
+      <a
+        href="https://www.instagram.com/nubila___/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="instagram-link"
+      >
+        <img
+          src={instagramIcon}
+          alt="Instagram"
+          style={{ width: "35px", height: "35px" }}
+        />
+      </a>
+      <a
+        href="https://open.spotify.com/artist/6fWZzJGm1glU7LAu374FSL?si=wsUOfKliQ9613oxyqYk7NA"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="spotify-link"
+      >
+        <img
+          src={spotifyIcon}
+          alt="Spotify"
+          style={{ width: "35px", height: "35px" }}
+        />
+      </a>
+    </div>
   );
 };
 

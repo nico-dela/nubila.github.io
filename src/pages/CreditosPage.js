69eecaca8d844796d70e91206fddec8ab937230e
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-10-17 23:59:57 -0300

    agregue estructura base para la pagina de creditos

--- src/pages/CreditosPage.js
+++ src/pages/CreditosPage.js
@@ -7,7 +7,28 @@ const CreditosPage = () => {
     <div className="container">
       <h1 className="heading">Créditos</h1>
       <div className="credits">
-        {/* Add your credit details here */}
+        <h2>Participantes del proyecto</h2>
+        <ul>
+          <li className="credit-item">Candela Gencarelli</li>
+          <li className="credit-item">Mateo Gencarelli</li>
+          <li className="credit-item">Tobias Gencarelli</li>
+          <li className="credit-item">Ulises Gencarelli</li>
+          <li className="credit-item">Araceli Bonfigli</li>
+          <li className="credit-item">Julian Medrano</li>
+          <li className="credit-item">Nicolás de la Cruz</li>
+        </ul>
+
+        <h2>Colaboradores Especiales</h2>
+        <ul>
+          <li className="credit-item">Nombre del Colaborador Especial 1</li>
+          <li className="credit-item">Nombre del Colaborador Especial 2</li>
+        </ul>
+
+        <h2>Agradecimientos</h2>
+        <p>
+          Agradecemos a todas las personas que contribuyeron de alguna manera al
+          proyecto.
+        </p>
       </div>
       <Link to="/nubila" className="link">
         <button className="button">Volver</button>

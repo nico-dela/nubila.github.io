862770d5d0e7dff9945fb7bb9975aa998a37b4dc
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-10-17 23:59:23 -0300

    estructura base para la pagina de colaboracion

--- src/pages/ColaboraPage.js
+++ src/pages/ColaboraPage.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../styles/CommonPage.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/ColaboraPage.css";
 
 const ColaboraPage = () => {
   return (
     <div className="container">
-      <h1 className="heading">Colaborá con el proyecto</h1>
-      <div className="credits">
-        {/* Add your credit details here */}
+      <h1 className="heading">Colaborá</h1>
+      <div className="collaborate">
+        <strong>¡Gracias por asomarte!</strong>
+        <p>
+          Cada contribución, grande o pequeña, nos ayuda a financiar
+          grabaciones, producciones en vivo y otras actividades que nos permiten
+          seguir haciendo lo que amamos.
+        </p>
+        <p>Si queres colaborar, podés hacerlo de las siguientes formas:</p>
+        <ul>
+          <li>
+            Contribuir a nuestro proyecto a través de donaciones en línea.
+          </li>
+          <li>Asistir a nuestros conciertos y/o comprar merchandising.</li>
+          <li>
+            Compartir nuestra música y promocionar el proyecto en las redes
+            sociales.
+          </li>
+        </ul>
+        <p>
+          Tu apoyo es esencial para continuar creando y
+          compartiendo arte.
+        </p>
       </div>
       <Link to="/nubila" className="link">
         <button className="button">Volver</button>

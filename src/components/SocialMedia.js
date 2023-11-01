12499b617fd608f16df7ecdc187df6f710cb8f75
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-10-31 23:33:06 -0300

    unifique acceso a redes sociales

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,39 +1,18 @@
 import React from "react";
-import SocialMediaIcon from "./SocialMediaIcon";
+import socialMediaIcon from "../assets/images/social-media.png";
 import "../styles/SocialMedia.css";
-
-const socialMediaLinks = [
-  {
-    name: "Instagram",
-    url: "https://www.instagram.com/nubila___/",
-    className: "instagram-link",
-  },
-  {
-    name: "Spotify",
-    url: "https://open.spotify.com/artist/6fWZzJGm1glU7LAu374FSL?si=wsUOfKliQ9613oxyqYk7NA",
-    className: "spotify-link",
-  },
-  {
-    name: "Youtube",
-    url: "https://www.youtube.com/@nubila5910",
-    className: "youtube-link",
-  },
-];
 
 const SocialMedia = () => {
   return (
-    <div id="main-container">
-      {socialMediaLinks.map((link, index) => (
-        <a
-          key={index}
-          href={link.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`social-link ${link.className}`}
-        >
-          <SocialMediaIcon icon={link.name} />
-        </a>
-      ))}
+    <div className="social-container">
+      <a
+        href="https://linktr.ee/nubila"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-link"
+      >
+        <img src={socialMediaIcon} alt="Acceso a nuestras redes" />
+      </a>
     </div>
   );
 };

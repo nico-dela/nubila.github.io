ef1dc0edce6187d54ab0d3d569d21082b1a4f24f
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-10-31 23:43:16 -0300

    modifique informacion referente a los creditos

--- src/pages/CreditosPage.js
+++ src/pages/CreditosPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import lacasaLogo from "../assets/images/lacasa-logo.png";
 import "../styles/CreditosPage.css";
 
@@ -7,31 +6,59 @@ const CreditosPage = () => {
   return (
     <div className="container">
       <h1 className="heading">CREDITOS</h1>
-      <div className="credits">
-        <h2>Participantes del proyecto</h2>
-        <ul>
-          <li className="credit-item">Candela Gencarelli</li>
-          <li className="credit-item">Mateo Gencarelli</li>
-          <li className="credit-item">Tobias Gencarelli</li>
-          <li className="credit-item">Ulises Gencarelli</li>
-          <li className="credit-item">Araceli Bonfigli</li>
-          <li className="credit-item">Julian Medrano</li>
-          <li className="credit-item">Nicolás de la Cruz</li>
-        </ul>
+      <div className="credits-container">
+        <h3 style={{ fontSize: "1.5rem" }}>Diseño y arte</h3>
+        <div className="credit">
+          Portada y arte realizados por Tobias y Candela Gencarelli y Julian
+          Medrano
+        </div>
 
-        <h2>Colaboradores Especiales</h2>
-        <ul>
-          <li className="credit-item">Gregorio Lopez</li>
-        </ul>
+        <br></br>
+        <br></br>
 
-        <h2>Agradecimientos</h2>
-        <div className="item">
-          <img src={lacasaLogo} alt="La Casa Mutante Logo" className="logo" />
+        <h3 style={{ fontSize: "1.5rem" }}>Musicxs</h3>
+        <div className="credit">Voz, coros y bajo: Mateo Gencarelli</div>
+        <div className="credit">Voz, coros y teclados: Tobias Gencarelli</div>
+        <div className="credit">Voz y coros: Candela Gencarelli</div>
+        <div className="credit">Teclados: Ulises Gencarelli</div>
+        <div className="credit">Coros y batería: Araceli Bonfigli</div>
+        <div className="credit">Coros y saxo: Julian Medrano</div>
+        <div className="credit">Coros y guitarra: Nicolás de la Cruz</div>
+
+        <br></br>
+        <br></br>
+
+        <h3 style={{ fontSize: "1.5rem" }}>
+          Producción musical y postproducción de audio
+        </h3>
+        <div className="credit">
+          Técnica de grabación realizada por Gregorio López
+        </div>
+        <div className="credit">
+          Mezclas en HOME KONG studio por Mariano Garione y Tomás Salguero
+        </div>
+        <div className="credit">
+          Mastering en Surlumina Estudio por Fico Borgognone
+        </div>
+
+        <br></br>
+        <br></br>
+
+        <h3 style={{ fontSize: "1.5rem" }}>
+          Nubila forma parte del colectivo artístico
+        </h3>
+        <div>
+          <a
+            href={
+              "https://www.instagram.com/la.casa.mutante/?utm_source=ig_web_button_share_sheet&igshid=ZDNlZDc0MzIxNw=="
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={lacasaLogo} alt="La Casa Mutante Logo" className="logo" />
+          </a>
         </div>
       </div>
-      <Link to="/" className="link">
-        <button className="button">Volver</button>
-      </Link>
     </div>
   );
 };

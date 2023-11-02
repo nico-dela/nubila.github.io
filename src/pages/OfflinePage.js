ee0d2692a41ffeab0cb694f14ca9ce15f52c08fe
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-11-02 00:21:11 -0300

    modifique modal de carga

--- src/pages/OfflinePage.js
+++ src/pages/OfflinePage.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import bannerImage from "../assets/images/banner.jpeg";
-import loadingSpinner from "../assets/images/spinning-loading.gif"; // Agrega la imagen de la animación de carga
+import { Puff } from "react-loader-spinner";
 import "../styles/OfflinePage.css";
 
 const OfflinePage = () => {
@@ -8,22 +8,36 @@ const OfflinePage = () => {
 
   // Función para manejar el evento onLoad de la imagen
   const handleImageLoad = () => {
+    // Cuando la imagen se carga, actualiza el estado para indicar que la imagen se ha cargado.
     setImageLoaded(true);
   };
+
+  // Utiliza useEffect para verificar si la imagen ya está en caché y establecer imageLoaded en true
+  useEffect(() => {
+    const image = new Image();
+    image.src = bannerImage;
+    image.onload = handleImageLoad;
+  }, []);
 
   return (
     <div className="overlay">
       <div className="coming-soon-container">
         {imageLoaded ? (
           // La imagen se ha cargado, muestra la imagen
-          <img src={bannerImage} alt="Banner oceanica" />
+          <img
+            src={bannerImage}
+            alt="Banner oceanica"
+            onLoad={handleImageLoad}
+          />
         ) : (
           // La imagen aún se está cargando, muestra la animación de carga
-          <img
-            src={loadingSpinner}
-            alt="Loading"
-            className="loading-spinner"
-            onLoad={handleImageLoad} // Llama a la función handleImageLoad cuando la imagen se carga
+          <Puff
+            height={80}
+            width={80}
+            radius={1}
+            color="black"
+            ariaLabel="puff-loading"
+            visible={!imageLoaded} // Oculta el spinner cuando la imagen se carga
           />
         )}
       </div>

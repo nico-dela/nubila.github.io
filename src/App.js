0a2376888bc19244ebf5f47ca7fd5dcd02eaddf8
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-11-11 12:47:49 -0300

    agregue lazy loading para menu y reproductor de musica

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Suspense, lazy, useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import OceanicaPage from "./pages/OceanicaPage";
@@ -7,16 +7,17 @@ import BoleritoPage from "./pages/BoleritoPage";
 import MariposaPage from "./pages/MariposaPage";
 import FrioPage from "./pages/FrioPage";
 import LimoneroPage from "./pages/LimoneroPage";
-import SentimientoPage from "./pages/SentimientoPage";
 import CreditosPage from "./pages/CreditosPage";
 import ColaboraPage from "./pages/ColaboraPage";
 import OfflinePage from "./pages/OfflinePage";
-import MusicPlayer from "./components/MusicPlayer";
 import NotFoundPage from "./pages/NotFoundPage";
-import Menu from "./components/Menu";
 
 import "./App.css";
 import AcercaPage from "./pages/AcercaPage";
+import { Puff } from "react-loader-spinner";
+
+const Menu = lazy(() => import("./components/Menu"));
+const MusicPlayer = lazy(() => import("./components/MusicPlayer"));
 
 const App = () => {
   const [isOffline] = useState(false);
@@ -30,22 +31,35 @@ const App = () => {
           </Routes>
         ) : (
           <>
-            <Menu />
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/oceanica" element={<OceanicaPage />} />
-              <Route path="/girasoles" element={<GirasolesPage />} />
-              <Route path="/bolerito" element={<BoleritoPage />} />
-              <Route path="/mariposa" element={<MariposaPage />} />
-              <Route path="/frio" element={<FrioPage />} />
-              <Route path="/limonero" element={<LimoneroPage />} />
-              <Route path="/creditos" element={<CreditosPage />} />
-              {/* <Route path="/sentimiento" element={<SentimientoPage />} /> */}
-              <Route path="/colabora" element={<ColaboraPage />} />
-              <Route path="/acerca" element={<AcercaPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-            <MusicPlayer />
+            <Suspense
+              fallback={
+                <div className="loading-container">
+                  <Puff
+                    height={80}
+                    width={80}
+                    radius={1}
+                    color="#0A4066"
+                    ariaLabel="puff-loading"
+                  />
+                </div>
+              }
+            >
+              <Menu />
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/oceanica" element={<OceanicaPage />} />
+                <Route path="/girasoles" element={<GirasolesPage />} />
+                <Route path="/bolerito" element={<BoleritoPage />} />
+                <Route path="/mariposa" element={<MariposaPage />} />
+                <Route path="/frio" element={<FrioPage />} />
+                <Route path="/limonero" element={<LimoneroPage />} />
+                <Route path="/creditos" element={<CreditosPage />} />
+                <Route path="/colabora" element={<ColaboraPage />} />
+                <Route path="/acerca" element={<AcercaPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+              <MusicPlayer />
+            </Suspense>
           </>
         )}
       </div>
